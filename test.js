1f2134799e6afe6aacac3ff8f9ef63d069b9c48c
garicp <garicon@gmail.com>
2019-09-28 07:20:27 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TestingH = () => <h1 id="head">Testing... Testing...</h1>;
+
+ReactDOM.render(<TestingH />, document.getElementById('test'));
